c281853ebf7b79d3ff625927f1788cd53a2be700
rayudusk <rayudusundur24@gmail.com>
2021-03-21 11:07:41 +0530

    change in test mode

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,4 +37,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+ 
+  @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "hello world";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
